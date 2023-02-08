2c217562de8dac0789f0e18bd01e36145b8ce813
muhamadsandi9096 <mssandi9096@gmail.com>
2023-02-08 14:37:30 +0700

    firts commit

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -17,11 +17,8 @@ final _satriaMuda = Golongan(
     'Sabuk Biru Belati 1',
     'Sabuk Biru Belati 2',
     'Sabuk Biru Belati 3',
-<<<<<<< HEAD
-    'Sabuk Biru Belati 4',
-=======
-    'Test Renz'
->>>>>>> c5ce3aadb0100a63a483062c31f34041d026e60c
+    'Test Renz',
+    'Test Sandi',
   ],
 );
 
