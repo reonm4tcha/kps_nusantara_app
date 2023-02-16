fdea2737333eca2a6930eec44f975c002a3ed271
Jabar <violetta.je.vymmiy15@gmail.com>
2023-02-16 13:45:52 +0700

    benerin gambar

--- lib/data/Sejarah_Singkat.dart
+++ lib/data/Sejarah_Singkat.dart
@@ -4,7 +4,7 @@ class Sejarah_Singkat {
   static String Pendiri = '1. Moch. Hadimulyo, B.Sc.\n2. Dr. Moh.Djoko Waspodo.\n3. Dr. Rachmadi Djoko Suwigyo.';
   static String Pendiri_Images = 'assets/image/Pendiri.png';
   static String Sebelumnya1 = 'assets/image/sebelunya1.jpg';
-  static String Sebelumnya2 = 'assets/image/sebelumnya2.png';
+  static String Sebelumnya2 = 'assets/image/Logo Study Group.png';
   static String Dahulu = '''Dahulu Perguruan ini Bernama  : \n“STUDY GROUP PENCAK SILAT NUSANTARA“ Perguruan ini di akui sebagai salah satu perguruan “TOP ORGANISASI PENCAK SILAT” 10 Perguruan Besar di Indonesia.''';
   static String maka_dari_itu = '''Maka dari itu Pada Tanggal 28 Juli 1973 Berganti Nama menjadi “KELUARGA PENCAKSILAT NUSANTARA”.  Dengan Pelatih Utama “ Moch. Hadimulyo, B.Sc.”''';
   static String KPN_image = 'assets/image/KPN.png';
