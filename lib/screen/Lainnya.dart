001a30d84b68bbdc8f2d1cfbde74ce5a3ace9b18
Jabar <violetta.je.vymmiy15@gmail.com>
2023-02-18 08:22:18 +0700

    add lainnya1

--- lib/screen/Lainnya.dart
+++ lib/screen/Lainnya.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ksp_nusantara_app/data/Lainnya_data.dart';
 
 class Lainnya extends StatefulWidget {
   const Lainnya({Key? key}) : super(key: key);
@@ -10,6 +11,65 @@ class Lainnya extends StatefulWidget {
 class _LainnyaState extends State<Lainnya> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      // backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('LAINNYA'),
+        centerTitle: true,
+        backgroundColor: Color(0xff362FD9),
+      ),
+      body: Material(
+        child: ListView(
+          children : [
+            Column(
+              children: [
+                // buildListLainnya()
+                ListView.builder(
+                    itemCount: Lainnya_data.length,
+                    itemBuilder: (context, index){
+                      final data = Lainnya_data[index];
+                      Container(
+                        width: double.infinity,
+                        height: 90,
+                        decoration: BoxDecoration(
+                            color: Colors.black12
+                        ),
+                        child:
+                        Row(
+                          children: [
+                            Text(data.name)
+                          ],
+                        ),
+                      );
+                    }
+                    )
+              ],
+            ),
+          ]
+        ),
+      ),
+    );
   }
+
+//   Widget buildListLainnya(){
+//     return ListView.builder(
+//         itemCount: Lainnya_data.length,
+//         itemBuilder: (context, index){
+//           final data = Lainnya_data[index];
+//           Container(
+//             width: double.infinity,
+//             height: 90,
+//             decoration: BoxDecoration(
+//               color: Colors.black12
+//             ),
+//             child:
+//             Row(
+//               children: [
+//                 Text(data.name)
+//               ],
+//             ),
+//           );
+//         }
+//     );
+//   }
 }
