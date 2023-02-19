4985fdd7041a4fafb67d3efd3c447ce1e4e086f7
Jabar <violetta.je.vymmiy15@gmail.com>
2023-02-19 08:33:01 +0700

    handling click Container

--- lib/widgets/profilPage.dart
+++ lib/widgets/profilPage.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 
-class profilPage extends StatefulWidget {
-  const profilPage({Key? key}) : super(key: key);
 
-  @override
-  State<profilPage> createState() => profilPageState();
-}
-
-class profilPageState extends State<profilPage> {
-
-  // profilPageState({});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 90,
-      color: Colors.blue,
-    );
-  }
+Widget profilPage(){
+  return ListView(
+    children: [
+      Container(
+        width: double.infinity,
+        height: 50,
+        color: Colors.black,
+      ),
+      Container(
+        width: double.infinity,
+        height: 50,
+        color: Colors.blue,
+      ),
+    ],
+  );
 }
