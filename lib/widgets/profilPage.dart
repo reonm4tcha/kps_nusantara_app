739163c4dc2514a91bcdef004443e3b5fb09d80c
Jabar <violetta.je.vymmiy15@gmail.com>
2023-02-21 09:28:12 +0700

    manage page v3

--- lib/widgets/profilPage.dart
+++ lib/widgets/profilPage.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 Widget profilPage(){
   return ListView(
     children: [
-      Center(
-        child: Text('Profilpage'),
-      )
+      Padding(
+        padding: EdgeInsets.only(top: 40),
+        child: Image.asset(
+          'assets/image/Perguruan.png',
+          height: 113,
+        ),
+      ),
     ],
   );
 }
