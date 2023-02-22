22527e040426ba34eb4ffbd6fc601ae432782d07
Jabar <violetta.je.vymmiy15@gmail.com>
2023-02-22 15:59:14 +0700

    manage page v11

--- lib/data/Data Lainnya/data AD DAN ART.dart
+++ lib/data/Data Lainnya/data AD DAN ART.dart
@@ -1,4 +1,5 @@
 class data_AD_DAN_ART{
+
   static String head_Pembukaan = 'I. PEMBUKAAN';
   static String isi_Pembukaan1 =
       '''Pencak Silat memiliki Peran Penting dalam berdirinya Bangsa Indonesia serta menjadikan Bangsa Indonesia sebagai Bangsa yang besar dan diakui seluruh Dunia, sebelum Negara Kesatuan Republik Indonesia ada Kerajaan-kerajaan di Tanah “NUSANTARA” mengukir sejarah yang sangat Luar biasa dengan para Pendekar-pendekar Pencak Silatnya yang dapat menguasai Kerajaan-keraja di Luar Kawasan Nusantara hingga Manca Negara, sehingga Pantaslah disebut, Bahwa Pencak Silat adalah salah satu Warisan Budaya Nasional dan merupakan hasil krida Bangsa Indonesia yang memiliki nilai-nilai yang sangat luhur. Kemudian secara turun temurun Pencak Silat dilestarikan dan dikembangkan sesuai dengan Aspirasi (keinginan), Ekspesasi (harapan), Apresiasi (penilaian) dalam situasi dan kondisi yang berkembang dari generasi ke generasi maupun dari waktu ke waktu.''';
@@ -11,4 +12,11 @@ class data_AD_DAN_ART{
   '''Kesatria Pewaris Silat Nusantara sebagai kelanjutan dan pembaharuan Pencak Silat Moderen Bangsa Indonesia, dibentuk berdasarkan tanggungjawab moral atas kelestarian Budaya Bangsa Indonesia yang ditopang oleh empat Pondasi Wawasan Kebangsaan, yaitu :''';
   static String isi_Pembukaan6 = '- Idiologi Pancasila;n\- Undang – Undang Dasar 1945;\n- Bhinneka Tunggal Ika; dan\n- Negara Kasatuan Republik Indonesia.';
   static String isi_Pembukaan7 = 'Dengan Rakhmat Tuhan Yang Maha Esa, maka disusunlah Anggaran Dasar dan Anggaran Rumah Tangga Kesatria Pewaris Silat Nusantara sebagai berikut :';
+
+  static String BAB_I = 'BAB I';
+  static String jenis_pasalBABI = 'Nama, Waktu dan Tempat';
+  static String no_1BABI = 'Pasal 1';
+  static String nama_passal1BABI = 'Nama';
+  static String isi_passalBABI =
+  '''Perguruan ini bernama Kesatria Pewaris Silat Nusantara atau disingkat “Kesatria PS Nusantara”. Kesatria Pewaris Silat Nusantara didirikan sebagai Penerus Estafet dari Kesatria PS Nusantara yang di Pelopori Almarhum Moch. Hadi Mulyo, B.Sc berawal, pada tanggal 28 Juli 1968 dengan nama "Studi Group Pencak Silat Nusantara" untuk kemudian pada tanggal 28 Juli 1973 berganti nama menjadi Keluarga Pencak Silat Nusantara, dalam rangka melestarikan Keilmuan Almarhum Moch. Hadi Mulyo,B.Sc serta pengamalan Catur Prasetya Nusantara maka pada tanggal 25 Februari 2018 terbentuklah pembaharuan  "Kesatria Pewaris Silat Nusantara"., Namun tetap mengacu pada hari Jadinya “Studi Group Pencak Silat Nusantara” Tanggal 28 Juli 1968.''';
 }
